63845c1b83d98ee578aedf760c8b31814895e039
paulorayann <paulorpsena@gmail.com>
2022-09-22 13:36:43 -0300

    feat: add getProductByID method function

--- src/handlers.ts
+++ src/handlers.ts
@@ -20,3 +20,25 @@ export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGat
   };
 };
 
+export const getProductByID = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+  const id = event.pathParameters?.id
+
+  const output = await docClient.get({
+    TableName: 'Products',
+    Key: {
+      productID: id
+    }
+  }).promise()
+
+  if (!output.Item) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: 'Product not found' }),
+    };
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(output.Item),
+  }
+}
