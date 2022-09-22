4ed4c326001a4bd60aee1bee685c805ca534eb71
paulorayann <paulorpsena@gmail.com>
2022-09-22 14:41:34 -0300

    feat: add createProduct fields validation with yup

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,18 +1,31 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import AWS from 'aws-sdk'
 import { v4 } from "uuid";
+import * as yup from 'yup'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const headers = {
   'content-type': 'application/json',
 }
+const product = yup.object().shape({
+  name: yup.string().trim().required(),
+  description: yup.string().trim().required(),
+  price: yup.number().min(0.5).max(1000).required(),
+  inStock: yup.boolean().required(),
+})
 
 export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
   const reqBody = JSON.parse(event.body as string)
+
+  const {error} = await product.validate(reqBody, { abortEarly: false })
+  if (error) throw error
+
   const productBody = {
     ...reqBody,
     productID: v4(),
   }
+
   await docClient.put({
     TableName: 'Products',
     Item:  productBody
@@ -22,7 +35,17 @@ export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGat
     headers,
     body: JSON.stringify(productBody),
   };
-};
+
+  } catch (error) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        message: error.message,
+      }),
+    };
+  }
+}
 
 export const getProductByID = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
